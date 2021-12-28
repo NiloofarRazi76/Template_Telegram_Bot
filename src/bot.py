df5b6041e28fd100e40548532e105fe97f9e9013
NiloofarRz <razi.niloofar99civileng@gmail.com>
2021-12-28 19:32:12 +0330

    add base code for telegram bot

---
+++ src/bot.py
@@ -0,0 +1,40 @@
+import os
+
+import emoji
+import telebot
+from loguru import logger
+from telebot import types
+
+from src.constants import keyboards
+from src.utils.io import read_json, write_json
+
+
+class Bot:
+	"""
+	Telegram bot to randomly connect two strangers to talk
+	"""
+	def __init__(self):
+		self.bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
+		self.echo_all = self.bot.message_handler(
+			func=lambda m: True
+			)(self.echo_all)
+
+	def run(self):
+		logger.info('Bot is running...')
+		self.bot.infinity_polling()
+
+	def echo_all(self, message):
+		#write_json(message.json, "message.json")
+		print(emoji.demojize(message.text))
+		# self.bot.reply_to(message, message.text)
+		self.bot.send_message(
+			message.chat.id, message.text,
+			#reply_markup=keyboards.main,
+			#reply_markup=create_keyboard(message.chat.first_name)
+			reply_markup=keyboards.main
+			)
+
+if __name__ == '__main__':
+	logger.info('Bot started')
+	bot = Bot()
+	bot.run()
---
+++ src/utils/keyboard.py
@@ -0,0 +1,16 @@
+from telebot import types
+import emoji
+
+def create_keyboard(*keys, row_width=2, resize_keyboard=True):
+    """
+    Create a keyboard from a list of keys.
+    """
+    markup = types.ReplyKeyboardMarkup(
+        row_width=row_width,
+        resize_keyboard=resize_keyboard
+    )
+    keys = map(emoji.emojize, keys)
+    buttons = map(types.KeyboardButton, keys)
+    markup.add(*buttons)
+
+    return markup
